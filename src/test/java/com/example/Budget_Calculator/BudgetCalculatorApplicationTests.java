546f22e87ddcb2219e3d3f578950c929ab2dcbb7
Kyle Lohrberg <lorbs32@gmail.com>
2025-02-22 13:30:06 -0500

    created project

---
+++ src/test/java/com/example/Budget_Calculator/BudgetCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Budget_Calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
